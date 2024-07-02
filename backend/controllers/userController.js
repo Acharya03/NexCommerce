aa5b65397e95fa5f16bbadc4092f7e0130a2c857
Arunabho Bhattacharya <barunabho@gmail.com>
2024-07-02 09:55:10 +0530

    user token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import bcrypt from "bcryptjs";
+import createToken from '../utils/createToken.js';
+
 
 const createUser = asyncHandler(async(req, res) => {
     const {username, email, password} = req.body;
@@ -20,6 +22,7 @@ const createUser = asyncHandler(async(req, res) => {
 
     try{
         await newUser.save();    //mongoose command that allows us to store new users in the database.
+        createToken(res, newUser._id);
 
         res.status(201).json({_id: newUser._id, username: newUser.username, email: newUser.email, isAdmin: newUser.isAdmin,});
     }
