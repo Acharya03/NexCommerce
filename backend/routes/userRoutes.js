7d903a43149327b4a2b406c6e7a3125451a8c663
Arunabho Bhattacharya <barunabho@gmail.com>
2024-11-16 11:10:10 +0530

    Added 'update user profile' route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile } from "../controllers/userController.js";
+import { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile, updateCurrentUserProfile } from "../controllers/userController.js";
 
 import { authenticate,authorizeAdmin } from "../middlewares/authMiddleware.js";
 
@@ -9,7 +9,7 @@ router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
 
-router.route("/profile").get(authenticate, getCurrentUserProfile);
+router.route("/profile").get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
 
 
 export default router;
