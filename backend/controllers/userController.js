eb1d3b5de80cac4a1e046beae49eb4209c711342
Arunabho Bhattacharya <barunabho@gmail.com>
2024-11-16 11:44:19 +0530

    upd admin authorized to delete user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,7 +78,7 @@ const getCurrentUserProfile = asyncHandler(async(req, res) => {
     }
     else{
         res.status(404);
-        throw new Error("User not found.");
+        throw new Error("User not found");
     }
 });
 
@@ -110,4 +110,22 @@ const updateCurrentUserProfile = asyncHandler(async(req, res) => {
     }
 });
 
-export { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile,  updateCurrentUserProfile};
+const deleteUserById = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if(user){
+        if(user.isAdmin){
+            res.status(400);
+            throw new Error("Cannot delete Admin user");
+        }
+
+        await User.deleteOne({_id: user._id});
+        res.json({message: "User removed"})
+    }
+    else{
+        res.status(404);
+        throw new Error("User not found");
+    }
+});
+
+export { createUser, loginUser, logoutCurrentUser, getAllUsers, getCurrentUserProfile,  updateCurrentUserProfile, deleteUserById};
