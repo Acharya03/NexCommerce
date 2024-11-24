99155d1e71eea4c5ef17322c43ed913bbad543d7
Arunabho Bhattacharya <barunabho@gmail.com>
2024-11-24 18:10:17 +0530

    api upd

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -11,24 +11,48 @@ import { FaHeart } from 'react-icons/fa';
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./Navigation.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useLoginMutation } from "../../redux/api/usersApiSlice";
+import { logout } from "../../redux/features/auth/authSlice";
+
 
 
 const Navigation = () => {
+
+    const { userInfo } = useSelector(state => state.auth);
+
+
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [showSidebar, setShowSidebar] = useState(false);
 
     const toggleDropdown = () => {
         setDropdownOpen(!dropdownOpen);
-    }
+    };
 
     const toggleSidebar = () => {
         setShowSidebar(!showSidebar);
-    }
+    };
 
     const closeSidebar = () => {
         setShowSidebar(false);
+    };
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLoginMutation();
+
+    const logoutHandler = async() => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate("/login");
+        } catch (error) {
+            console.error(error);
+        }
     }
+
 
     return (
         <div style={{ zIndex: 999 }} className={`${showSidebar ? "hidden" : "flex"} 
@@ -57,6 +81,18 @@ const Navigation = () => {
                 </Link>
             </div>
 
+            <div className="relative">
+                <button onClick={toggleDropdown} className="flex items-center text-gray-800 focus:outline-none">
+                    {userInfo ? (
+                        <span className="text-black">
+                            {userInfo.username}
+                        </span>
+                    ) : (
+                        <></>
+                    )}
+                </button>
+            </div>
+
             <ul>
                 <li>
                     <Link to='/login' className="flex items-center transition-transform 
@@ -69,12 +105,12 @@ const Navigation = () => {
                     <Link to='/register' className="flex items-center transition-transform 
                 transform hover:translate-x-2">
                         <AiOutlineUserAdd size={26} className="mr-2 mt-[3rem]" />
-                        <span className="hidden nav-item-name mt-[3rem]">Register</span>
+                        <span className="hidden nav-item-name mt-[3rem]">Register</span>{" "}
                     </Link>
                 </li>
             </ul>
         </div>
-    )
-}
+    );
+};
 
 export default Navigation;
